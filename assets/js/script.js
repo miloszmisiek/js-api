ed87f38fbdfead722ed3d1195cc71018117b0d24
miloszmisiek <milosz.misiek@gmail.com>
2022-03-14 18:28:39 +0000

    Add displayException function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,20 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function displayException(data) {
+
+    let heading = "An Exception Occured";
+
+    results = `<div>The API returned status code ${data.status_code}</div>`;
+    results += `<div>Error number: <b>${data.error_no}</b></div>`;
+    results += `<div>Error text: <b>${data.error}</b></div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+
+    resultsModal.show();
+}
+
 function displayErrors(data) {
 
     let heading = `JSHint Results for ${data.file}`;
@@ -36,12 +50,29 @@ async function getStatus(e) {
     if (response.ok) {
         displayStatus(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 }
 
+function processOptions(form) {
+
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === "options") {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -56,6 +87,7 @@ async function postForm(e) {
     if (response.ok) {
         displayErrors(data);
     } else {
+        displayException(data);
         throw new Error(data.error);
     }
 
